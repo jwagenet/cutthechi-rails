7121235b6aa96ff8a86c9d6db71491fd19736557
Jonathan Wagenet <jwagenet@charter.net>
2014-09-14 23:49:21 -0700

    Tima again

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,9 +19,9 @@ module ApplicationHelper
 	end
 
 	def date_check
-		if Time.now < Time.new(2014,9,15).in_time_zone("Pacific Time (US & Canada)")
+		if Time.now.in_time_zone("Pacific Time (US & Canada)") < Time.new(2014,9,15).in_time_zone("Pacific Time (US & Canada)")
 			return 0
-		elsif Time.now > Time.new(2014,9,19,12).in_time_zone("Pacific Time (US & Canada)")
+		elsif Time.now.in_time_zone("Pacific Time (US & Canada)") > Time.new(2014,9,19,12).in_time_zone("Pacific Time (US & Canada)")
 			return 1
 		else
 			return 2
