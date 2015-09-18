aafe125bc52a46f70e54e010f427cd8fbd806c30
Jonathan Wagenet <jwagenet@charter.net>
2015-09-17 23:12:31 -0700

    stef

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,10 @@
 #User.create(name: "Jonathan Wagenet", email: "j_wagenet@u.pacific.edu", phone: "5418409908", password: "admin1", admin: "1") 
 
 
-#Candidate.create(name: "Emanual Rodriguez", photo: "erodriguez", comment: "I'll remember your face.") 
+Candidate.create(name: "Stefan Soezeri", photo: "ssoezeri", comment: "Because my girlfriend made me.") 
 
 #Candidate.find_by(photo: "bchen").update_columns(comment: "Help me reach my dream of becoming the next Dalai Lama.")
 #Candidate.find_by(photo: "tosterchrist").update_columns(comment: "I just want a haircut.")
 #Candidate.find_by(photo: "jshih").destroy
-User.find_by(name:"karly").update_columns(name: "Karly and Tri Delta")
+#User.find_by(name:"karly").update_columns(name: "Karly and Tri Delta")
 
